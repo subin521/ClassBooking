bcacfe33a91343b26c002ff8c562a4c0f897f09a
JangWookKyeong <jwkyeong4107@gmail.com>
2025-04-28 00:26:42 +0900

    관리자 id, 유효성 검사 로직 추가

--- ClassBooking/ClassBooking.cpp
+++ ClassBooking/ClassBooking.cpp
@@ -196,7 +196,7 @@ bool isValidPassword(const std::string &pw);
 bool loadUsers()
 {
     ifstream fin("user.txt"); // .cpp용
-    //ifstream fin("../../Classbooking/ClassBooking/user.txt");     // release .exe용
+    //ifstream fin("../../Classbooking/ClassBooking/user.txt"); // release .exe용
 
     if (!fin)
     {
@@ -207,7 +207,7 @@ bool loadUsers()
             exit(1);
         }
         fout.close();
-        return true;
+        // 파일 만들어주고 진행
     }
 
     string id, pw;
@@ -233,14 +233,15 @@ bool loadUsers()
             cerr << "[Error] Duplicate ID detected in user.txt -> " << id << endl;
             exit(1);
         }
-
         id_check[id] = true;
 
         bool isAdmin = (admin != 0);
-        users.push_back({id, pw, isAdmin});
+        users.push_back({ id, pw, isAdmin });
     }
-
     fin.close();
+
+    users.push_back({ "admin1", "admin123", true });
+
     return true;
 }
 
